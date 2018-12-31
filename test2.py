9c347e6be814016c360a94e6c25716eb37c7a4df
Kevin Chang <kevin@fishyfish.local>
2018-12-30 22:48:30 -0600

    Add main check to test2.py.

--- test2.py
+++ test2.py
@@ -1,6 +1,11 @@
+# Print all elements in 'elems' one per row.
 def print_elems(elems):
 	for elem in elems:
 		print(elem)
 
-x = [1, 2]
-print_elems(x)
+def main():
+  x = [1, 2]
+  print_elems(x)
+
+if __name__ == '__main__':
+  main()
