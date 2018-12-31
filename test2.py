276b26c086de275a0c21d684e492b17404450acc
Kevin Chang <kevin@kchang.me>
2018-12-30 23:11:21 -0600

    Add in math plotting to test2.py.

--- test2.py
+++ test2.py
@@ -1,11 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 # Print all elements in 'elems' one per row.
 def print_elems(elems):
 	for elem in elems:
 		print(elem)
 
 def main():
-  x = [1, 2]
-  print_elems(x)
+  X = np.linspace(-np.pi, np.pi, 256, endpoint=True)
+  C,S = np.cos(X), np.sin(X)
+
+  plt.plot(X,C)
+  plt.plot(X,S)
+
+  plt.show()
 
 if __name__ == '__main__':
   main()
